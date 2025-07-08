3b4e59b640d6fce8b089c813be5e68a0968f8798
Bruno Miranda Tessilla <brutusmt0811@gmail.com>
2025-07-07 23:25:36 -0300

    trying

--- index.py
+++ index.py
@@ -1,7 +1,22 @@
 from flask import Flask, jsonify, render_template, request, flash, redirect, url_for
+from flask_oidc import OpenIDConnect
 
 
 app = Flask(__name__, template_folder = 'pages')
+app.config['SECRET_KEY'] = 'super secret key'
+
+app.config.update({
+    'TESTING': True,
+    'DEBUG': True,
+    'OIDC_CLIENT_SECRETS': 'auth.json',
+    'OIDC_ID_TOKEN_COOKIE_SECURE': True,
+    'OIDC_USER_INFO_ENABLED': True,
+    'OIDC_OPENID_REALM': 'guess',
+    'OIDC_SCOPES': ['openid', 'profile'],
+    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret',
+})
+
+oidc = OpenIDConnect(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def cadastrar():
@@ -13,4 +28,4 @@ def cadastrar():
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
-    app.run(port=3000, debug=True)
+    app.run(port=3000, debug=False)
